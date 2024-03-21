43d2e5914d1460bacf7a3ab03edd9d77462cb745
EC2 Default User <ec2-user@ip-172-31-30-127.ap-northeast-1.compute.internal>
2024-03-21 14:48:26 +0000

    commit

---
+++ neighbor.rb
@@ -0,0 +1,15 @@
+n, m = gets.split.map(&:to_i)
+
+a = Array.new(m)
+m.times{|i| a[i] = gets.split.map(&:to_i)}
+
+answer = Array.new(n) { [] }
+
+a.each do |j|
+    c, d = j
+    answer[c - 1] << d
+    answer[d - 1] << c
+end
+
+answer.map! { |neighbors| neighbors.empty? ? [-1] : neighbors }
+answer.each { |neighbors| puts neighbors.join(' ') }
